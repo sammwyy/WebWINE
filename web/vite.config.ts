e0db5ce1a0e6e105a3701a811873c7a4db4af287
Sammwy <sammwy.dev@gmail.com>
2026-06-05 01:29:08 -0300

    fix(gh-pages): assets loading

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/webwine/" : "/",
+  base: mode === "production" ? "/WebWINE/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
